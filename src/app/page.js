203a3e2435a0407e8a9e2a769e4f2fe2f5b62921
Victor Nguyen <victornguyen0702@gmail.com>
2025-02-21 00:54:58 +1100

    feat: add tech stack

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,19 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import ProjectCarousel from "@/components/ui/ProjectCarousel";
 import Header from "@/components/ui/Header"
+import TechStack from "@/components/TechStack";
 export default function Home() {
   return (
     
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
+
       <div>
         
       </div>
-      
+      <TechStack />
       <ProjectCarousel />
 
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
-      </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
       
