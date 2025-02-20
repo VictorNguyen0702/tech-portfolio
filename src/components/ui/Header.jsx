9bda6db4a0bfba259ebe5889395b6131e20e5c97
Victor Nguyen <victornguyen0702@gmail.com>
2025-02-20 22:24:23 +1000

    fix: change material UI website to ShadCN ui

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Github, Linkedin } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 w-full grid grid-cols-3 items-center p-4 bg-[var(--light-color)] text-white shadow-lg">
+      {/* Empty space for balancing */}
+      <div></div>
+
+      <h1 className="text-xl font-bold bg-[var(--light-color)] text-[var(--dark-color)] text-center">Victor's Portfolio</h1>
+
+      <div className="flex justify-end gap-4">
+        <a href="https://github.com/VictorNguyen0702">
+          <Button
+              variant="outline"
+              className="text-white border-white bg-[var(--dark-color)] hover:bg-[var(--dark-color)] hover:text-[var(--light-color)]">
+              <Github className="w-5 h-5 mr-2"/>
+              Github
+          </Button>
+        </a>
+        <a href="https://www.linkedin.com/in/victor-nguyen-43123b26a/">
+          <Button
+              variant="outline"
+              className="text-white border-white bg-[var(--dark-color)] hover:bg-[var(--dark-color)] hover:text-[var(--light-color)]">
+              <Linkedin className="w-5 h-5 mr-2" />
+              LinkedIn
+          </Button>
+        </a>
+      </div>
+    </header>
+
+
+  );
+}
