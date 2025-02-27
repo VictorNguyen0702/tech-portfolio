828e890373016b5a29625078442b5d41a7a1fb6c
Victor Nguyen <victornguyen0702@gmail.com>
2025-02-27 14:24:32 +1100

    fix: change techboard title colour

--- src/components/ui/TechBoard.jsx
+++ src/components/ui/TechBoard.jsx
@@ -17,7 +17,7 @@ export default function Technologies() {
   return (
       <div className="p-6 md:p-8 bg-[var(--light-color)] rounded-xl border border-neutral-800 w-full max-w-[90vw] sm:max-w-lg mx-auto">
 
-        <h3 className="text-2xl font-semibold mb-8 text-center">
+        <h3 className="text-2xl text-[var(--dark-color)] font-semibold mb-8 text-center">
           Languages and Frameworks
         </h3>
 
