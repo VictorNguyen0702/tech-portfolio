d1d1d02d77da235af93ebcdcec630d51e51a4083
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-13 22:14:45 +1100

    feat: add hero background image

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -15,10 +15,10 @@ const Hero = () => {
       });
     }
     return (
-      <div className="bg-[var(--hero-color)]">
+      <div className="bg-cover brightness-75" style={{ backgroundImage: "url('/background-image.png')" }}>
         <FadeIn className="flex flex-col items-left justify-left px-10 py-20 lg:p-20 gap-5">
-          <div className="text-3xl font-bold sm:w-full lg:w-[30%]">Computer Science Student and Aspiring Backend Developer</div>
-          <div className="text-lg sm:w-full lg:w-[30%]">Tech enthusiast and software developer who loves turning ideas into functional applications</div>
+          <div className="text-3xl text-[var(--hero-text-color)] font-bold sm:w-full lg:w-[30%]">Computer Science Student and Aspiring Backend Developer</div>
+          <div className="text-lg text-[var(--hero-text-color)] sm:w-full lg:w-[30%]">Tech enthusiast and software developer who loves turning ideas into functional applications</div>
           <Button asChild className="bg-[var(--dark-color)] text-white max-w-[150px] rounded-full px-4 py-2 transition hover:brightness-75">
             <motion.a href="#projects" className="hover:text-[var(--text-hover-color)]" onClick={(e) => { e.preventDefault(); handleScroll('projects');}}>Projects</motion.a>
           </Button>
